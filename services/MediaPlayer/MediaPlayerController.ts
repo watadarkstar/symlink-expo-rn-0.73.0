4f9d8702077806627183aaeff9808c062b376b34
Adrian Carolli <adrian.caarolli@gmail.com>
2024-04-02 17:23:54 -0400

    fix: add the method registerAudioPlayerPlaybackService

--- services/MediaPlayer/MediaPlayerController.ts
+++ services/MediaPlayer/MediaPlayerController.ts
@@ -2,6 +2,10 @@ class MediaPlayerController {
   public play() {
     console.log("play");
   }
+
+  public registerAudioPlayerPlaybackService() {
+    console.log("registerAudioPlayerPlaybackService");
+  }
 }
 
 export default new MediaPlayerController();
